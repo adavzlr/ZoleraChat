c322f86227e3a819b4d8c2b614f6cb8f09d806a5
vidalortiz <vidalortiz@users.noreply.github.com>
2015-02-12 23:55:25 -0600

    Part of the previous refactoring
    I don't know why this wasn't committed as part of the previous commit.

--- Infrastructure/src/zolera/chat/infrastructure/IChatServer.java
+++
@@ -1,9 +0,0 @@
-package zolera.chat.infrastructure;
-
-import java.rmi.*;
-
-public interface IChatServer
-extends Remote {
-	public IChatRoom reference(String roomname)
-	throws RemoteException;
-}
